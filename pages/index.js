c9e2572410e4a78d25e24185cbe6f3e2d17f7a0e
Thais Megumi <tha.megumi@gmail.com>
2023-10-04 07:48:50 -0300

    add `lint:check` and `lint:fix` scripts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Amigos tornam as risadas mais altas, as conversas mais gostosas e a vida mais FELIZ ❤️</h1>
+  return (
+    <h1>
+      Amigos tornam as risadas mais altas, as conversas mais gostosas e a vida
+      mais FELIZ ❤️
+    </h1>
+  );
 }
 
 export default Home;
